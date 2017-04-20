a892d60b2af29c0cc140a6abd5085b34f56e65fe
GuerraUTFPR <guerramatheus2@gmail.com>
2017-04-20 17:50:05 -0300

    models_cid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420203817) do
+ActiveRecord::Schema.define(version: 20170420204552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170420203817) do
     t.string   "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cids", force: :cascade do |t|
+    t.string   "cod_doenca"
+    t.string   "nome_doenca"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "pessoas", force: :cascade do |t|
