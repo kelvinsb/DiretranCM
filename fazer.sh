0a9b4e8eb56226a744798c9ef87e7934b705fdc5
kelvinsb <kelvinbr11@gmail.com>
2017-04-20 18:52:44 -0300

    Criação model: Estado

--- fazer.sh
+++ fazer.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-rake db:drop
-rake db:create
-rake db:migrate
-rails generate scaffold Estado nome:string uf:string
-rails generate scaffold Funcionario
-rails generate scaffold Bairro nome:string
-rails generate scaffold Documentos rg:string cpf:string cid:string foto:string comp_residencia:string
-rails generate scaffold Categoria nome:string
-#rake db:schema:load
-rake db:migrate
-
-
-
-git checkout -b create_models_3
+#rake db:drop
+#rake db:create
+#rake db:migrate
+#rails generate scaffold Estado nome:string uf:string
+#rake db:migrate
 git add .
-git commit -m "motivo"
+git commit -m "Criação model: Estado"
 git push origin create_models_3
+
+#rails generate scaffold Funcionario
+#rails generate scaffold Bairro nome:string
+#rails generate scaffold Documentos rg:string cpf:string cid:string foto:string comp_residencia:string
+#rails generate scaffold Categoria nome:string
+#rake db:schema:load
+#rake db:migrate
+
+#git checkout -b create_models_3
+#git add .
+#git commit -m "motivo"
+#git push origin create_models_3
