d8cc41fc6df4db18a80376510911584196b3f195
kelvinsb <kelvinbr11@gmail.com>
2017-04-20 18:55:27 -0300

    Criação model: Funcionario

--- fazer.sh
+++ fazer.sh
@@ -4,14 +4,30 @@
 #rake db:migrate
 #rails generate scaffold Estado nome:string uf:string
 #rake db:migrate
+#git add .
+#git commit -m "Criação model: Estado"
+#git push origin create_models_3
+
+rails generate scaffold Funcionario
 git add .
-git commit -m "Criação model: Estado"
+git commit -m "Criação model: Funcionario"
 git push origin create_models_3
 
-#rails generate scaffold Funcionario
-#rails generate scaffold Bairro nome:string
-#rails generate scaffold Documentos rg:string cpf:string cid:string foto:string comp_residencia:string
-#rails generate scaffold Categoria nome:string
+rails generate scaffold Bairro nome:string
+git add .
+git commit -m "Criação model: Bairro"
+git push origin create_models_3
+
+rails generate scaffold Documentos rg:string cpf:string cid:string foto:string comp_residencia:string
+git add .
+git commit -m "Criação model: Documentos"
+git push origin create_models_3
+
+rails generate scaffold Categoria nome:string
+git add .
+git commit -m "Criação model: Categoria"
+git push origin create_models_3
+
 #rake db:schema:load
 #rake db:migrate
 
