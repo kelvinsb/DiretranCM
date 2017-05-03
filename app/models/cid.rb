fc099006332c7d95dbc66c64146ca6b6e59ce6e3
GuerraUTFPR <guerramatheus2@gmail.com>
2017-05-03 02:21:42 -0300

    validações

--- app/models/cid.rb
+++ app/models/cid.rb
@@ -1,3 +1,9 @@
 class Cid < ApplicationRecord
-	belongs_to :carteirinha
+	#belongs_to :carteirinha
+
+	validates :cod_doenca,
+	presence: true
+
+    validates :nome_doenca,
+    presence: true
 end
