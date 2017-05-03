edbdae2fdc459d9cfd3a798680b70fc4ff86fb94
RafaelAlessandroRamos <alessandro.leafar@gmail.com>
2017-05-03 00:42:58 -0300

    validação pessoa

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -2,4 +2,18 @@ class Pessoa < ApplicationRecord
 	has_many :login
 	belongs_to :funcionario
 	belongs_to :requisicao
+
+	validates :nome, presence: {message: ": deve ser preenchido!"},
+									 length: {maximum: 255}
+
+	validates :cpf, presence: {message: ": deve ser preenchido!"},
+									length: {minimum: 11, maximum: 11},
+									numericality: {only_integer: true},
+									uniqueness: true
+ validates_cpf_format_of :cpf, message: 'inválido inserido'
+
+	validates :rg, presence: {message: ": deve ser preenchido!"},
+									length: {minimum: 7, maximum: 7},
+									numericality: {only_integer: true}
+
 end
