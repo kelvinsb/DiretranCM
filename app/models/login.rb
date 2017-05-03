8da64f772dc564e37651b97a129c5450adceba8a
GuerraUTFPR <guerramatheus2@gmail.com>
2017-05-03 01:10:54 -0300

    validação login

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,2 +1,17 @@
 class Login < ApplicationRecord
+	#belongs_to: funcionario
+	#belongs_to: pessoa
+
+	validates :nome,
+	presence: true,
+	uniqueness: true,
+  	length: { minimum: 5, maximum: 50 }
+  	#exclusion: { in: %w(admin), message: "%{value} é um usuário reservado"}
+
+ 	validates :senha,
+ 	presence: true,
+ 	length: {minimum: 8}
+ 	
+   	#has_secure_password
+
 end
