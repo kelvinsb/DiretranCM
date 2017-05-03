1b049dc5c4f1ea513d6f6003f7e509af65112bb1
RafaelAlessandroRamos <alessandro.leafar@gmail.com>
2017-05-03 02:14:38 -0300

    validações

--- app/models/estado.rb
+++ app/models/estado.rb
@@ -1,3 +1,12 @@
 class Estado < ApplicationRecord
 	belongs_to :endereco
+
+	validates :nome,
+	presence: {message: ": deve ser preenchido!"},
+	length: {maximum: 20}
+
+	validates :uf,
+	presence: {message: ": deve ser preenchido!"},
+	length: {minimum: 2, maximum: 2}
+
 end
