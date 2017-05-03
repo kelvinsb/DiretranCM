7ee1749ada08475742769cbf75175f853d8513b0
GuerraUTFPR <guerramatheus2@gmail.com>
2017-05-03 01:44:08 -0300

    validação requisição

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,19 +1,21 @@
 class Pessoa < ApplicationRecord
 	has_many :login
-	belongs_to :funcionario
-	belongs_to :requisicao
+	#belongs_to :funcionario
+	#belongs_to :requisicao
 
-	validates :nome, presence: {message: ": deve ser preenchido!"},
-									 length: {maximum: 255}
+	validates :nome,
+	presence: {message: ": deve ser preenchido!"},
+	length: {maximum: 255}
 
-	validates :cpf, presence: {message: ": deve ser preenchido!"},
-									length: {minimum: 11, maximum: 11},
-									numericality: {only_integer: true},
-									uniqueness: true
- validates_cpf_format_of :cpf, message: 'inválido inserido'
+	validates :cpf,
+	presence: {message: ": deve ser preenchido!"},
+	length: {minimum: 11, maximum: 11},
+	numericality: {only_integer: true},
+	uniqueness: true
+ 	validates_cpf_format_of :cpf, message: 'inválido inserido'
 
 	validates :rg, presence: {message: ": deve ser preenchido!"},
-									length: {minimum: 7, maximum: 7},
-									numericality: {only_integer: true}
+	length: {minimum: 9, maximum: 9},
+	numericality: {only_integer: true}
 
 end
--- app/models/requisicao.rb
+++ app/models/requisicao.rb
@@ -5,4 +5,30 @@ class Requisicao < ApplicationRecord
 	has_one :documentos
 	has_one :carteirinha
 
+	validates :responsavel_nome,
+	presence: true,
+	length: { minimum: 5, maximum: 50 }
+
+	validates :responsavel_cpf,
+	presence: {message: ": deve ser preenchido!"},
+	length: {minimum: 11, maximum: 11},
+	numericality: {only_integer: true},
+	uniqueness: true
+ 	validates_cpf_format_of :responsavel_cpf, message: 'inválido'
+
+ 	validates :endereco_id,
+ 	presence: true
+
+ 	validates :documentos_id,
+ 	presence: true
+
+    validates :pessoa_id,
+    presence: true
+
+    validates :funcionario_id,
+    presence: true
+
+    validates :carteirinha_id,
+    presence: false
+
 end
