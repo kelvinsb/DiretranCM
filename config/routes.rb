8918db913bc37fc940bd8a51e1ea59b9f364066e
JenniferIzabel <izabeljennifer@gmail.com>
2017-05-18 20:16:53 -0300

    mudança do index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   root 'dash#index'
 
-  devise_for :administradors
+ # devise_for :administradors
   devise_for :usuarios
+
+  devise_scope :usuario do
+     root to: "devise/sessions#new"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
