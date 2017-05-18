edd31512688ca1e3be77d471267df3c1d72df375
JenniferIzabel <izabeljennifer@gmail.com>
2017-05-18 06:26:16 -0300

    inicial vazia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
 
   # !LER redirecionar, mudar ainda
   def after_sign_in_path_for(resource)
-  	new_cadastro_path
+  	new_pessoa_path
   end
+
 end
