5233f51a20981706015af7f03277ca219cec674d
GuerraUTFPR <guerramatheus2@gmail.com>
2017-05-18 05:30:11 -0300

    correção de validações/inernacionalização(pt-BR)/ajuste de view

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,14 +1,13 @@
 class Pessoa < ApplicationRecord
   belongs_to :usuario
 
-  validates :nome,
+  	validates :nome,
 	presence: {message: ": deve ser preenchido"},
 	length: {maximum: 255}
 
 	validates :rg,
 	presence: {message: ": deve ser preenchido"},
-	length: {minimum:9, maximum:9, message: ": deve ser preenchido com 9 digitos"},
-	numericality: {only_integer: true, message: ": deve ser preenchido com números"}
+	length: {minimum:9, maximum:9, message: ": deve ser preenchido com 9 digitos"}	
 
 	validates :cpf,
 	presence: {message: ": deve ser preenchido"},
@@ -20,13 +19,15 @@ class Pessoa < ApplicationRecord
 	validates :data_nascimento,
 	presence: {message: ": deve ser preenchido"}
 
-  validates :telefone,
-	presence: {message: ": deve ser preenchido"}
+  	validates :telefone,
+	presence: {message: ": deve ser preenchido"},
+	numericality: {only_integer: true, message: ": deve ser preenchido com números"}
 
 	validates :celular,
-	presence: {message: ": deve ser preenchido"}
+	presence: {message: ": deve ser preenchido"},
+	numericality: {only_integer: true, message: ": deve ser preenchido com números"}
 
 	validates :usuario_id,
-  uniqueness: true
+  	uniqueness: true
 
 end
