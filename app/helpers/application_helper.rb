67addce20204ec7c6799e87e12754ea38c677109
kelvinsb <kelvinbr11@gmail.com>
2017-05-18 19:36:54 -0300

    Reescrita do cabeçalho e dash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	#def getPessoaId()
+	#	@Pessoa = Pessoa.all
+	#	@return = 0
+	#	@Pessoa.each do |p|
+	#		if p.usuario_id == current_usuario.id
+	#			@return = p.id
+	#		end
+	#	end
+	#	@return
+	#end
 end
