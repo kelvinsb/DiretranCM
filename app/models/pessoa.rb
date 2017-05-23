0f8b6e8c91a7df83f781cdd60a4c81a231bdc387
RafaelAlessandroRamos <alessandro.leafar@gmail.com>
2017-05-22 22:33:42 -0300

    desobrigação de campos do cadastro

--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,13 +1,13 @@
 class Pessoa < ApplicationRecord
   belongs_to :usuario
 
-  	validates :nome,
+  validates :nome,
 	presence: {message: ": deve ser preenchido"},
 	length: {maximum: 255}
 
 	validates :rg,
 	presence: {message: ": deve ser preenchido"},
-	length: {minimum:8, maximum:14, message: ": deve ser preenchido com 9 digitos"}	
+	length: {minimum:8, maximum:14, message: ": deve ser preenchido com 9 digitos"}
 
 	validates :cpf,
 	presence: {message: ": deve ser preenchido"},
@@ -19,14 +19,14 @@ class Pessoa < ApplicationRecord
 	validates :data_nascimento,
 	presence: {message: ": deve ser preenchido"}
 
-  	validates :telefone,
-	presence: {message: ": deve ser preenchido"},
-	numericality: {only_integer: true, message: ": deve ser preenchido com números"}
+  validates :telefone,
+	#presence: {message: ": deve ser preenchido"},
+	numericality: {only_integer: true}
 
 	validates :celular,
 	presence: {message: ": deve ser preenchido"},
-	numericality: {only_integer: true, message: ": deve ser preenchido com números"}
+	numericality: {only_integer: true}
 
 	validates :usuario_id,
-  	uniqueness: true
+  uniqueness: true
 end
