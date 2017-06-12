ab8c2bfe97220bf11dc551e0de0fbf289ff1f5d9
GuerraUTFPR <guerramatheus2@gmail.com>
2017-06-12 19:41:21 -0300

    remoção do cpf do modelo pessoa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606004816) do
+ActiveRecord::Schema.define(version: 20170612222612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 20170606004816) do
     t.string   "nome"
     t.string   "sexo"
     t.string   "rg"
-    t.string   "cpf"
     t.date     "data_nascimento"
     t.string   "telefone"
     t.string   "celular"
