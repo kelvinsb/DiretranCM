81de2b33b3da67670addb58990b785f4426a3f3a
GuerraUTFPR <guerramatheus2@gmail.com>
2017-06-12 19:37:31 -0300

    removendo cpf do modelo pessoa

--- db/migrate/20170531220734_email_to_cpf.rb
+++ db/migrate/20170531220734_email_to_cpf.rb
@@ -1,5 +1,6 @@
 class EmailToCpf < ActiveRecord::Migration[5.0]
   def change
   	 rename_column :usuarios, :email, :cpf
-  	 add_column :usuarios, :email, :cpf
+  	 #add_column :usuarios, :email, :cpf
+  end
 end
